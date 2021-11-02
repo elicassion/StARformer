c8195a9b10a9165b6dfc237b5e6c721e1b4c18a6
Jinghuan Shang <elicassion@users.noreply.github.com>
2021-11-02 19:37:02 -0400

    fix seed

--- atari/utils.py
+++ atari/utils.py
@@ -119,6 +119,8 @@ def set_seed(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
 
 def top_k_logits(logits, k):
     v, ix = torch.topk(logits, k)
